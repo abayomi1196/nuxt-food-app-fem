654c491666317233477a7ce65d156b7080b95d23
Abayomi Ayodele <samabayomi301@gmail.com>
2021-10-31 13:01:24 +0100

    initial commit with nuxt configs

---
+++ components/Tutorial.vue
@@ -0,0 +1,6 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <h1>Hello from Template</h1>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="container">
+    <h1>Hi from home page</h1>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
